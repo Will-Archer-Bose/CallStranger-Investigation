143a199aabbfd9c2da1a068987683675ca9e1da3
Will Archer <66698752+Will-Archer-Bose@users.noreply.github.com>
2020-06-23 03:04:29 -0500

    Update amplification_factor.sh

--- amplification_factor.sh
+++ amplification_factor.sh
@@ -1,3 +1,6 @@
 tcpdump -A host 20.42.105.45 or host <device IP>
 
 # you will see the device IP printed to the console when you run CallStranger.py
+# use this to monitor the SUBSCRIBE and NOTIFY calls. 
+# Following the CallStranger Technical Report in @yunuscadirci's original repo,
+# you can calculate the potential amplification factor is the device being testing was used in a DDoS attack.
